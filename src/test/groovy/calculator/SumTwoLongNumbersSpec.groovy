50674ba40b32975e63d368ef4057b9ad59f454fc
Jegors Čemisovs <jegors_cemisovs@epam.com>
2022-11-18 23:03:29 +0200

    update test case

--- src/test/groovy/calculator/SumTwoLongNumbersSpec.groovy
+++ src/test/groovy/calculator/SumTwoLongNumbersSpec.groovy
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.CsvFileSource
+import org.junit.jupiter.params.provider.ValueSource
 
 @DisplayName("Calculator adds two long numbers")
 final class SumTwoLongNumbersSpec {
@@ -20,19 +21,18 @@ final class SumTwoLongNumbersSpec {
     @ParameterizedTest
     @CsvFileSource(resources = "sum-long-correct.csv", useHeadersInDisplayName = true)
     void 'returns the correct result if there is no overflow'(long a, long b, long expectedSum) {
-        def actualSum = calculator.sum a, b
+        def actualSum = calculator.sum(a, b)
 
         Assertions.assertEquals expectedSum, actualSum
     }
 
     @ParameterizedTest
-    @CsvFileSource(resources = "sum-long-incorrect.csv")
-    void 'returns the wrong result on overflow'(long a, long b) {
-        def expectedSum = a + b as BigInteger
+    @ValueSource(longs = [1L, 99L, 1_983_322L, 8_923_021_768L, Long.MAX_VALUE])
+    void 'returns an incorrect result if the sum of the numbers exceeds the maximum Long'(long a) {
+        def sum = Long.MAX_VALUE + 1
+        def b = (sum - a) as long
+        def actualSum = calculator.sum(a, b) as BigInteger
 
-        def actualSum = calculator.sum a, b
-
-        Assertions.assertNotEquals expectedSum, actualSum
+        Assertions.assertNotEquals sum, actualSum
     }
-
 }
