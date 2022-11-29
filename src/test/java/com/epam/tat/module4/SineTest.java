eac8a960c87e5ceca3e95cd542ad3cf1d3043a75
Jegors Čemisovs <jegors_cemisovs@epam.com>
2022-11-29 20:10:40 +0200

    add sine test

--- src/test/java/com/epam/tat/module4/SineTest.java
+++ src/test/java/com/epam/tat/module4/SineTest.java
@@ -15,24 +15,25 @@ import static java.lang.Math.sqrt;
 import static org.junit.jupiter.params.provider.Arguments.arguments;
 
 @Execution(ExecutionMode.CONCURRENT)
-@DisplayName("Calculator calculates tangent")
-final class TangentTest extends CalculatorTest {
+@DisplayName("Calculator calculates sine")
+final class SineTest extends CalculatorTest {
 
-    private static Stream<Arguments> tangentSampleData() {
+    private static Stream<Arguments> sineSampleData() {
         return Stream.of(
                 arguments(0, 0),
-                arguments(PI / 6, sqrt(3) / 3),
-                arguments(PI / 4, 1),
-                arguments(PI / 3, sqrt(3)),
+                arguments(PI / 6, 1./2),
+                arguments(PI / 4, sqrt(2) / 2),
+                arguments(PI / 3, sqrt(3) / 2),
+                arguments(PI / 2, 1),
                 arguments(PI, 0)
         );
     }
 
-    @ParameterizedTest(name = "tangent {0, number, 0.00} is {1, number, 0.00}")
-    @MethodSource("tangentSampleData")
-    @DisplayName("check for a number that is positive")
+    @ParameterizedTest(name = "sine {0, number, 0.00} is {1, number, 0.00}")
+    @MethodSource("sineSampleData")
+    @DisplayName("calculates sine for a real number")
     void checkLongPositiveNumber(double radian, double expected) {
-        var actual = calculator.tg(radian);
+        var actual = calculator.sin(radian);
         Assertions.assertEquals(expected, actual, DELTA);
     }
 }
