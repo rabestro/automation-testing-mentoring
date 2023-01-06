8a4327bac41a3b8d5efd357aa38f2107cb31c23b
Jegors Čemisovs <jegors_cemisovs@epam.com>
2023-01-06 23:36:34 +0200

    add ByModelSelect

---
+++ selenium-pages/src/main/java/com/epam/engx/selenium/pages/gcpc/ByModelSelect.java
@@ -0,0 +1,36 @@
+package com.epam.engx.selenium.pages.gcpc;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.SearchContext;
+import org.openqa.selenium.WebElement;
+
+import java.util.List;
+
+public final class ByModelSelect extends By {
+    private final By locator;
+
+    public ByModelSelect(String model) {
+        var css = "md-select[ng-model$='%s']".formatted(model);
+        locator = By.cssSelector(css);
+    }
+
+    @Override
+    public List<WebElement> findElements(SearchContext context) {
+        return context.findElements(locator);
+    }
+
+    @Override
+    public String toString() {
+        return locator.toString();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        return super.equals(o);
+    }
+
+    @Override
+    public int hashCode() {
+        return super.hashCode();
+    }
+}
