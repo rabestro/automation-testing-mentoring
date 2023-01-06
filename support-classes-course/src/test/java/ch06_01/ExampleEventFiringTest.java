916c511e40626817c61b0fc29103dbc38342ba03
Jegors Čemisovs <jegors_cemisovs@epam.com>
2023-01-06 18:05:43 +0200

    replace deprecated class with actual

--- support-classes-course/src/test/java/ch06_01/ExampleEventFiringTest.java
+++ support-classes-course/src/test/java/ch06_01/ExampleEventFiringTest.java
@@ -6,35 +6,36 @@ import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.events.AbstractWebDriverEventListener;
-import org.openqa.selenium.support.events.EventFiringWebDriver;
+import org.openqa.selenium.support.events.EventFiringDecorator;
+import org.openqa.selenium.support.events.WebDriverListener;
 
 import static org.assertj.core.api.Assertions.catchThrowable;
 import static org.assertj.core.api.BDDAssertions.then;
 
 @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
 final class ExampleEventFiringTest {
-    WebDriver driver;
+    private WebDriver driver;
 
     @BeforeEach
-    public void createDriver() {
+    void createDriver() {
         driver = new ChromeDriver();
-
         driver.get("https://eviltester.github.io/supportclasses/");
-
     }
 
     @Test
     void logging_finding_elements() {
+        // given
         var resend = By.id("resend-select");
         var noSuchElement = By.id("no-such-element");
 
-        var events = new EventFiringWebDriver(driver);
-        events.register(new LocalEventFiringListener());
+        var events = new EventFiringDecorator<>(new LocalEventFiringListener())
+                .decorate(driver);
 
+        // when
         var resendElem = events.findElement(resend);
 
-        then(resendElem).isNotNull();
+        then(resendElem)
+                .isNotNull();
 
         // when
         var thrown = catchThrowable(
@@ -49,20 +50,19 @@ final class ExampleEventFiringTest {
 
 
     @AfterEach
-    public void closeDriver() {
+    void closeDriver() {
         driver.quit();
     }
 
-    private static class LocalEventFiringListener extends AbstractWebDriverEventListener {
-
+    public static class LocalEventFiringListener implements WebDriverListener {
         @Override
-        public void beforeFindBy(By by, WebElement element, WebDriver driver) {
-            System.out.println("Looking For " + by.toString());
+        public void beforeFindElement(WebDriver driver, By locator) {
+            System.out.println("Looking For " + locator.toString());
         }
 
         @Override
-        public void afterFindBy(By by, WebElement element, WebDriver driver) {
-            System.out.println("Finished looking for " + by.toString());
+        public void afterFindElement(WebDriver driver, By locator, WebElement result) {
+            System.out.println("Finished looking for " + locator.toString());
         }
     }
 }
