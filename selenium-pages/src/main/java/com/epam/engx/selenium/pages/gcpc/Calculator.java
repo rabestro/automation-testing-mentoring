79d400e6883babf905ad87e8a69914275081c5db
Jegors Čemisovs <jegors_cemisovs@epam.com>
2023-01-08 21:12:19 +0200

    update Calculator

--- selenium-pages/src/main/java/com/epam/engx/selenium/pages/gcpc/Calculator.java
+++ selenium-pages/src/main/java/com/epam/engx/selenium/pages/gcpc/Calculator.java
@@ -1,10 +1,33 @@
 package com.epam.engx.selenium.pages.gcpc;
 
+import com.epam.engx.selenium.pages.gcpc.model.Model;
+
 import java.util.Map;
 
+/**
+ * Public calculator API used in tests
+ */
 public interface Calculator {
 
+    /**
+     * Parameter model used in the calculator
+     *
+     * @param model - name to search for a model in the DOM
+     * @return the model found by name
+     */
     Model model(String model);
 
-    Map<String, String> params();
+    /**
+     * Current values of all parameters in the calculator
+     *
+     * @return returns a map containing the name of the models with their value
+     */
+    Map<String, String> parameters();
+
+    /**
+     * Estimation of the monthly rent for current parameters
+     *
+     * @return an estimate of the cost of the virtual machine
+     */
+    Estimate estimate();
 }
