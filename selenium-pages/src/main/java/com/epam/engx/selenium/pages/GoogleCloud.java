a67e201b00515a75dd0f230888a09dfaa7da6531
Jegors Čemisovs <jegors_cemisovs@epam.com>
2023-01-08 21:12:29 +0200

    update GoogleCloud

--- selenium-pages/src/main/java/com/epam/engx/selenium/pages/GoogleCloud.java
+++ selenium-pages/src/main/java/com/epam/engx/selenium/pages/GoogleCloud.java
@@ -3,15 +3,18 @@ package com.epam.engx.selenium.pages;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 
 // page_url = https://cloud.google.com/
-public final class GoogleCloud extends AbstractPage {
+public final class GoogleCloud extends PageFactory {
+    private final WebDriver driver;
 
     @FindBy(name = "q")
     private WebElement searchInput;
 
     private GoogleCloud(WebDriver driver) {
-        super(driver);
+        this.driver = driver;
+        initElements(driver, this);
     }
 
     public static GoogleCloud openPage(WebDriver driver) {
