044beafa3031a773349075bec829486561b1e421
Jegors Čemisovs <jegors_cemisovs@epam.com>
2023-01-08 21:12:45 +0200

    update SearchResult

--- selenium-pages/src/main/java/com/epam/engx/selenium/pages/SearchResult.java
+++ selenium-pages/src/main/java/com/epam/engx/selenium/pages/SearchResult.java
@@ -3,12 +3,15 @@ package com.epam.engx.selenium.pages;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 
+import java.time.Duration;
 import java.util.List;
 import java.util.function.Function;
 
 // page_url = https://cloud.google.com/s/results?q=Google%20Cloud%20Platform%20Pricing%20Calculator
-public class SearchResult extends AbstractPage {
+public class SearchResult {
+    private final WebDriver driver;
 
     @FindBy(xpath = "//a[b]")
     private List<WebElement> results;
@@ -17,14 +20,16 @@ public class SearchResult extends AbstractPage {
     private WebElement firstResult;
 
     public SearchResult(WebDriver driver) {
-        super(driver);
+        this.driver = driver;
+        PageFactory.initElements(driver, this);
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
     }
 
     public List<Link> links() {
         return results.stream().map(Link::new).filter(Link::hasText).toList();
     }
 
-    public <T extends AbstractPage> T goFirst(Function<? super WebDriver, T> pageFabric) {
+    public <T> T goFirst(Function<? super WebDriver, T> pageFabric) {
         firstResult.click();
         return pageFabric.apply(driver);
     }
