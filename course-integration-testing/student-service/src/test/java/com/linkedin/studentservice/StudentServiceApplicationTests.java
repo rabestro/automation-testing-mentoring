f5237a9261d0c768632865c7fe4a8437308ecbff
Jegors Čemisovs <jegors_cemisovs@epam.com>
2023-01-12 21:27:42 +0200

    add StudentServiceApplicationTest

---
+++ course-integration-testing/student-service/src/test/java/com/linkedin/studentservice/StudentServiceApplicationTests.java
@@ -0,0 +1,14 @@
+package com.linkedin.studentservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentServiceApplicationTests
+{
+
+	@Test
+	void contextLoads() {
+	}
+
+}
