aac15496030ca03d34681844e12a27ed91a43a9b
Jegors Čemisovs <jegors_cemisovs@epam.com>
2023-01-13 16:19:30 +0200

    add Student

---
+++ course-integration-testing/student-client/src/main/java/com/linkedin/studentclient/Student.java
@@ -0,0 +1,14 @@
+package com.linkedin.studentclient;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Student {
+    private Long id;
+    private String studentName;
+    private int grade;
+}
