e0b6208f9e69ea859ab45d8d573ceda530a018ad
Jegors Čemisovs <jegors_cemisovs@epam.com>
2023-01-19 03:13:55 +0200

    add BaseLocalTest

---
+++ selenium-pages/src/test/java/com/epam/engx/selenium/pages/BaseLocalTest.java
@@ -0,0 +1,28 @@
+package com.epam.engx.selenium.pages;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.util.Objects;
+
+public abstract class BaseLocalTest {
+    protected static WebDriver driver;
+
+    @BeforeAll
+    protected static void setUp() {
+        driver = new FirefoxDriver();
+    }
+
+    @AfterAll
+    protected static void tearDown() {
+        driver.manage().deleteAllCookies();
+        driver.quit();
+    }
+
+    protected void openHtmlFile(String htmlFile) {
+        var url = getClass().getResource(htmlFile);
+        driver.get("file://" + Objects.requireNonNull(url).getPath());
+    }
+}
