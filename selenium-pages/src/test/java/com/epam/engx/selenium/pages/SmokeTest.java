fd03d8d9ba15751d58812c4c47ecab74600cd8c9
Jegors Čemisovs <jegors_cemisovs@epam.com>
2023-01-22 23:41:45 +0200

    add SmokeTestSuite

--- selenium-pages/src/test/java/com/epam/engx/selenium/pages/SmokeTest.java
+++ selenium-pages/src/test/java/com/epam/engx/selenium/pages/SmokeTest.java
@@ -8,7 +8,7 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-@Target({ElementType.METHOD, ElementType.TYPE})
+@Target(ElementType.METHOD)
 @Retention(RetentionPolicy.RUNTIME)
 @Tag("SmokeTest")
 @Test
