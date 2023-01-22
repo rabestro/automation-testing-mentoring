a7dcde74b753438be86b21143030cb4b6c615325
Jegors Čemisovs <jegors_cemisovs@epam.com>
2023-01-22 21:59:25 +0200

    add SmokeTest

---
+++ selenium-pages/src/test/java/com/epam/engx/selenium/pages/SmokeTest.java
@@ -0,0 +1,16 @@
+package com.epam.engx.selenium.pages;
+
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target({ElementType.METHOD, ElementType.TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+@Tag("SmokeTest")
+@Test
+public @interface SmokeTest {
+}
