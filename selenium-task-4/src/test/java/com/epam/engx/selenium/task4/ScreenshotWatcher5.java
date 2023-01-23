7b71bf734a7b34da09bb337cccb9de435ae57b51
Jegors Čemisovs <jegors_cemisovs@epam.com>
2023-01-23 10:10:17 +0200

    add ScreenshotWatcher5::getCurrentTimeAsString

--- selenium-task-4/src/test/java/com/epam/engx/selenium/task4/ScreenshotWatcher5.java
+++ selenium-task-4/src/test/java/com/epam/engx/selenium/task4/ScreenshotWatcher5.java
@@ -10,12 +10,14 @@ import org.openqa.selenium.WebDriverException;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.Optional;
 
-public class ScreenshotWatcher5 implements TestWatcher {
+public final class ScreenshotWatcher5 implements TestWatcher {
 
-    WebDriver driver;
-    String path;
+    private final WebDriver driver;
+    private final String path;
 
     public ScreenshotWatcher5(WebDriver driver, String path) {
         this.driver = driver;
@@ -34,7 +36,6 @@ public class ScreenshotWatcher5 implements TestWatcher {
 
     @Override
     public void testFailed(ExtensionContext context, Throwable throwable) {
-        // do something
         captureScreenshot(driver, context.getDisplayName());
     }
 
@@ -46,11 +47,18 @@ public class ScreenshotWatcher5 implements TestWatcher {
     public void captureScreenshot(WebDriver driver, String fileName) {
         try {
             new File(path).mkdirs();
-            try (FileOutputStream out = new FileOutputStream(path + File.separator + "screenshot-" + fileName + ".png")) {
+            var filePath = path + File.separator + "screenshot-" + getCurrentTimeAsString() + "-" + fileName + ".png";
+            try (var out = new FileOutputStream(filePath)) {
                 out.write(((TakesScreenshot) driver).getScreenshotAs(OutputType.BYTES));
             }
         } catch (IOException | WebDriverException e) {
             System.out.println("screenshot failed:" + e.getMessage());
         }
     }
+
+    private String getCurrentTimeAsString(){
+        var formatter = DateTimeFormatter.ofPattern( "uuuu-MM-dd_HH-mm-ss" );
+        return ZonedDateTime.now().format(formatter);
+    }
 }
+
