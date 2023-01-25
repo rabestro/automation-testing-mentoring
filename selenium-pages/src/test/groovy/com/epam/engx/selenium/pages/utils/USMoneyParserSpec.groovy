5ab5f49058d38267df8909fa3a4a5050b2408778
Jegors Čemisovs <jegors_cemisovs@epam.com>
2023-01-25 22:40:26 +0200

    add SelectComponentSpec

--- selenium-pages/src/test/groovy/com/epam/engx/selenium/pages/utils/USMoneyParserSpec.groovy
+++ selenium-pages/src/test/groovy/com/epam/engx/selenium/pages/utils/USMoneyParserSpec.groovy
@@ -3,6 +3,7 @@ package com.epam.engx.selenium.pages.utils
 import spock.lang.Narrative
 import spock.lang.Specification
 import spock.lang.Subject
+import spock.lang.Tag
 import spock.lang.Title
 
 @Title('The Page Object parses money representation on web page')
@@ -11,22 +12,24 @@ As a Page Object
 I want to parse the USD money representation of the estimated monthly cost
 So that I am able to provide a model of bill estimation for the test.
 ''')
+@Tag('fast')
 class USMoneyParserSpec extends Specification {
 
     def 'parse usd money representation'() {
-        given:
+
+        given: 'parser for string representation of dollar amount'
         @Subject def parser = new USMoneyParser()
 
-        when:
+        when: 'we process the string representation of the amount in US dollars'
         def money = parser.apply representation
 
-        then:
+        then: 'we get the money object with the correct amount and currency'
         with(money) {
             numberStripped == amount
             currency.currencyCode == 'USD'
         }
 
-        where:
+        where: 'monthly rent in US dollars'
         representation  | amount
         'USD 1,080.20'  | 1080.20
         'USD 1000000'   | 1000000
